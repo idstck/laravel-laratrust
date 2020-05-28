3d2a0046a6b3f701f3b6c8017896f20121bddc80
idstack <idstack@outlook.co.id>
2020-05-28 04:09:14 +0000

    membuat component blade alert

---
+++ app/Traits/FlashAlert.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Traits;
+
+/**
+ * Flash Alert Notification
+ */
+trait FlashAlert
+{
+    public function alertCreated()
+    {
+        return [
+            'type' => 'success',
+            'message' => 'Data successfully created!'
+        ];
+    }
+
+    public function alertUpdated()
+    {
+        return [
+            'type' => 'success',
+            'message' => 'Data successfully updated!'
+        ];
+    }
+
+    public function alertDeleted()
+    {
+        return [
+            'type' => 'success',
+            'message' => 'Data successfully deleted!'
+        ];
+    }
+
+    public function alertNotFound()
+    {
+        return [
+            'type' => 'warning',
+            'message' => 'Data not found!'
+        ];
+    }
+
+    public function alertDanger()
+    {
+        return [
+            'type' => 'danger',
+            'message' => 'Something wrong!'
+        ];
+    }
+
+    public function permissionDenied()
+    {
+        return [
+            'type' => 'danger',
+            'message' => 'you don’t have permission to access!'
+        ];
+    }
+}
